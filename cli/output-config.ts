4958d949d3260496941b5b933d6f64468b249b84
rdrdog <Redgwell@gmail.com>
2022-11-09 15:38:31 +1300

    adds s3 upload capability (#9)
    Co-authored-by: Robert Redgwell <robert@hypr.nz>

--- cli/output-config.ts
+++ cli/output-config.ts
@@ -1,35 +1,59 @@
 import { Command, Option } from "commander";
-import log from "../config/Logger";
+import log from "../config/logger";
+import { uploadToS3Async } from "../lib/s3-client";
 
 export function configureOutputModes(cmd: Command) {
   cmd
     .addOption(
       new Option("-f, --format <format>", "The format to output the data in.").default("JSON").choices(["JSON"])
     )
+    .addOption(
+      new Option("-t, --target <target>", "The destination to output the data to.")
+        .default("stdout")
+        .choices(["stdout", "s3"])
+    )
     .option(
-      "-t, --target <target>",
-      "The destination file to output the data to. If not specified then output will be echoed to the terminal."
+      "-d, --destination <destination>",
+      "The destination to output the data to. For target=s3, this is the name of the s3 bucket. For target=stdout, this does nothing."
+    )
+    .option(
+      "-k, --key <key>",
+      "The key to use for the destination. For target=s3, this is the name of the file in the s3 bucket. For target=stdout, this does nothing."
     );
 }
 
-export function outputData(options: OutputOptions, data: Object) {
-  let result;
+export async function outputDataAsync(options: OutputOptions, data: Object) {
+  let result, contentType;
   switch (options.format.toLowerCase()) {
     case "json":
       result = JSON.stringify(data, null, 2);
+      contentType = "application/json";
       break;
     default:
       throw new Error(`invalid output format specified: '${options.format}'.`);
   }
 
-  if (!options.target) {
-    log.info("\n" + result);
-  } else {
-    log.warn(`TODO - output to file at ${options.target}`);
+  switch (options.target.toLowerCase()) {
+    case "stdout":
+      log.info("\n" + result);
+      break;
+    case "s3":
+      if (!options.destination) {
+        throw new Error(`invalid destination specified to upload the data to s3: '${options.destination}'.`);
+      }
+      if (!options.key) {
+        throw new Error(`invalid key specified to upload the data to s3: '${options.key}'.`);
+      }
+      await uploadToS3Async(options.destination, options.key, result, contentType);
+      break;
+    default:
+      throw new Error(`invalid target specified: '${options.target}'.`);
   }
 }
 
 export interface OutputOptions {
   format: string;
   target: string;
+  destination?: string;
+  key?: string;
 }
